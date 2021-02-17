32f097e529588e4162e83a7e3c9204e51b5ac3a6
Braden Michelson <63371610+Braden50@users.noreply.github.com>
2021-02-17 08:37:58 -0800

    Updated DB models in backend

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,11 @@
 from datetime import datetime
 from mongoengine import Document, EmbeddedDocument
-from mongoengine.fields import DateTimeField, ReferenceField, StringField, IntField, ListField, PointField, FloatField
+from mongoengine.fields import DateTimeField, ReferenceField, StringField, IntField, ListField, PointField, FloatField, BooleanField
+
+# Note: Boolean field seems to exist in documentation, but not in autocomplete here
+
+class Settings(Document):
+    autoAddToGroupPortfolio = BooleanField(default=True)
 
 class User(Document):
     meta = {"collection": "user"}
@@ -9,7 +14,7 @@ class User(Document):
     date_joined = DateTimeField(default=datetime.now)
     metrics = ReferenceField("UserMetrics", dbref=True)
     achievements = ListField(ReferenceField("Achievement"), default=list)
-    portfolio = ReferenceField("Portfolio", dbref=True)
+    portfolio = ReferenceField("PersonalPortfolio", dbref=True)
 
 class UserMetrics(EmbeddedDocument):
     works_visited = IntField(default=0)
@@ -26,10 +31,11 @@ class Achievement(Document):
 class Artwork(Document):
     meta = {"collection": "artwork"}
     title = StringField(required=True)
+    artist = StringField(required=True)
     description = StringField(required=True)
-    found_by = ReferenceField(User)
-    metrics = ReferenceField("ArtworkMetrics", dbref=True)
+    found_by = ReferenceField("User")
     location = PointField()
+    metrics = ReferenceField("ArtworkMetrics", dbref=True)
     rating = FloatField(min_value=0, max_value=100)
     comments = ListField(ReferenceField("Comment"), default=list)
     tags = ListField(StringField(), default=list)
@@ -39,9 +45,26 @@ class ArtworkMetrics(EmbeddedDocument):
 
 class Comment(EmbeddedDocument):
     content = StringField(required=True)
-    author = ReferenceField(User, required=True)
+    author = ReferenceField("User", required=True)
+    # could add int attribute for likes
 
-class Portfolio(Document):
+class PersonalPortfolio(Document):
     meta = {"collection" : "portfolio"}
-    owner = ReferenceField(User)
-    artworks = ListField(ReferenceField(Artwork), default=list)
+    owner = ReferenceField("User")
+    artworks = ListField(ReferenceField("Artwork"), default=list)
+
+class GroupPortfolio(Document):
+    meta = {"collection" : "portfolio"}
+    owner = ReferenceField("Group")
+    artworks = ListField(ReferenceField("Artwork"), default=list)
+
+class Group(Document):
+    meta = {"collection": "group"}
+    name = StringField(required=True)
+    bio = StringField()
+    members = ListField(ReferenceField("User"), default=list)
+    portfolio = ReferenceField("GroupPortfolio")  # unsure how to use Portfolio object here
+    chat = ListField(ReferenceField("Comment"), default=list)     # this is replacing the "ChatRoom object" for now
+
+
+
