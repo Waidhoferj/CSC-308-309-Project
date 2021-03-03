0abc23792b146b8517e5ae7eeefe132d2a9d5dcb
Waidhoferj <jwaidhof@calpoly.edu>
2021-03-03 11:19:18 -0800

    Added top level sass styling file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ArtMap from "./pages/ArtMap/ArtMap";
 import Camera from "./pages/Camera/Camera";
