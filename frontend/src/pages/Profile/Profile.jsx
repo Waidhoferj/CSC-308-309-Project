f29199e9753fbbc447e9bfdc3b4ca21e65e815c0
Waidhoferj <jwaidhof@calpoly.edu>
2021-03-08 11:11:29 -0800

    added error messages

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,6 +1,7 @@
 import "./Profile.scss";
 import { useQuery, gql } from "@apollo/client";
-import { MoreHorizontal } from "react-feather";
+import { MoreHorizontal, AlertCircle } from "react-feather";
+import { useHistory } from "react-router-dom";
 import MetricBadge from "../../components/MetricBadge/MetricBadge";
 import exampleProfile from "../../assets/example-profile.jpg";
 
@@ -22,34 +23,46 @@ const GET_USER_QUERY = gql`
   }
 `;
 export default function Profile() {
-  const { data, loading } = useQuery(GET_USER_QUERY, {
+  const { data, error } = useQuery(GET_USER_QUERY, {
     variables: { userId: "0" },
   });
 
-  const user = loading ? {} : data?.users.edges[0].node;
+  const { goBack } = useHistory();
+
+  const user = data?.users.edges[0].node;
   return (
     <article className="Profile">
-      <header>
-        <div className="user">
-          <img src={exampleProfile} alt="Profile" />
-          <h1>{user?.name}</h1>
+      {error ? (
+        <div className="error-message">
+          <AlertCircle size={35} />
+          <p>Couldn't find user info</p>
+          <button onClick={goBack}>Go Back</button>
         </div>
-        <div className="options">
-          <button className="wrapper">
-            <MoreHorizontal size={30} />
-          </button>
-        </div>
-      </header>
-      <div className="content">
-        <div className="metrics">
-          <MetricBadge value={23} unit="Creations" />
-          <MetricBadge value={104} unit="Works Found" />
-          <MetricBadge value={2063} unit="Posts Written" />
-        </div>
+      ) : (
+        <>
+          <header>
+            <div className="user">
+              <img src={user?.profile} alt="Profile" />
+              <h1>{user?.name}</h1>
+            </div>
+            <div className="options">
+              <button className="wrapper">
+                <MoreHorizontal size={30} />
+              </button>
+            </div>
+          </header>
+          <div className="content">
+            <div className="metrics">
+              <MetricBadge value={23} unit="Creations" />
+              <MetricBadge value={104} unit="Works Found" />
+              <MetricBadge value={2063} unit="Posts Written" />
+            </div>
 
-        <h2>Bio</h2>
-        <p>{user?.bio}</p>
-      </div>
+            <h2>Bio</h2>
+            <p>{user?.bio}</p>
+          </div>
+        </>
+      )}
     </article>
   );
 }
