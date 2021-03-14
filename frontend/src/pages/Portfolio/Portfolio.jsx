18720fe3581a4e8bb94424c2d049b9c6e2fb7f08
Waidhoferj <jwaidhof@calpoly.edu>
2021-03-13 16:47:08 -0800

    added error messages

--- frontend/src/pages/Portfolio/Portfolio.jsx
+++ frontend/src/pages/Portfolio/Portfolio.jsx
@@ -3,6 +3,7 @@ import "./Portfolio.scss";
 import { gql, useQuery } from "@apollo/client";
 import { useMemo } from "react";
 import { useHistory } from "react-router-dom";
+import { AlertCircle, Search } from "react-feather";
 
 const GET_PORTFOLIO = gql`
   query getPortfolio($id: ID!) {
@@ -36,18 +37,27 @@ export default function Portfolio() {
       </header>
 
       <ul className="artworks">
-        {artworks?.map((artwork) => (
-          <ArtworkCard
-            key={artwork.title}
-            {...artwork}
-            onClick={() => history.push("/artwork/" + artwork.id)}
-          />
-        ))}
+        {!artworks ? (
+          <CannotFindArtworkMessage />
+        ) : !artworks.length ? (
+          <NoArtworkMessage />
+        ) : (
+          artworks?.map((artwork) => (
+            <ArtworkCard
+              key={artwork.title}
+              {...artwork}
+              onClick={() => history.push("/artwork/" + artwork.id)}
+            />
+          ))
+        )}
       </ul>
     </article>
   );
 }
 
+/**
+ * Represents single artwork in Portfolio
+ */
 function ArtworkCard({ title, picture, onClick }) {
   return (
     <li className="ArtworkCard" onClick={onClick}>
@@ -57,6 +67,36 @@ function ArtworkCard({ title, picture, onClick }) {
   );
 }
 
+function NoArtworkMessage() {
+  const history = useHistory();
+  return (
+    <div className="message">
+      <Search />
+      <p>
+        This is where you can view all of your discovered artwork. Start
+        exploring to fill your portfolio.
+      </p>
+      <button onClick={() => history.push("/map")}>Discover Art</button>
+    </div>
+  );
+}
+
+function CannotFindArtworkMessage() {
+  return (
+    <div className="message">
+      <AlertCircle />
+      <p>
+        Oops something went wrong. Check your connection and reload the page.
+      </p>
+    </div>
+  );
+}
+
+/**
+ * Grabs necessary artwork portfolio data from the server.
+ * @param {string} id
+ * @returns {{title: string, picture: string, id: string}} artwork portfolio info
+ */
 function usePortfolio(id) {
   const { data } = useQuery(GET_PORTFOLIO, { variables: { id } });
   const portfolio = useMemo(
