26093b5981cf4bc5826341b75ac84ca87e9066c6
Waidhoferj <jwaidhof@calpoly.edu>
2021-04-13 15:47:29 -0700

    basic discussion page

--- frontend/src/pages/Artwork/components/Discussion/Discussion.tsx
+++ frontend/src/pages/Artwork/components/Discussion/Discussion.tsx
@@ -1,10 +1,50 @@
 import "./Discussion.scss"
+import {useQuery, gql} from "@apollo/client"
+import { useMemo } from "react"
+import {useParams, useHistory} from "react-router-dom"
 
+import {
+    ArrowLeft,
+  } from "react-feather";
 
-export default function Discission() {
+const GET_DISCUSSION = gql`
+query getDiscussion($id: ID!) {
+  artwork(id: $id) {
+    edges {
+      node {
+        pictures
+        comments {
+          edges {
+            node {
+              content
+              datePosted
+              author {
+                  name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
 
+export default function Discussion() {
+    const {comments, picture} = useComments()
+    const {goBack} = useHistory()
+    console.log({comments, picture})
     return <article className="Discussion">
-
+        <header>
+        <img src={picture} alt="Art" />
+        <button className="wrapper back-button" onClick={goBack}>
+          <ArrowLeft />
+        </button>
+        <h1>Discussion</h1>
+      </header>
+      <ul className="content">
+            {comments.map(data => <Comment {...data}/>)}
+      </ul>
     </article>
 
 }
@@ -12,15 +52,42 @@ export default function Discission() {
 type CommentProps = {
     author: String,
     content: String,
-
+    datePosted: Date
 }
 
 
-function Comment() {
+function Comment(props: CommentProps) {
+    const dateOptions: Intl.DateTimeFormatOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
+    const dateStr:string = props.datePosted.toLocaleString("en", dateOptions)
     return <li className="Comment">
         <div className="metadata">
-            <p className="user"></p>
-            <p className="datetime"> { }</p>
+            <p className="user">{props.author}</p>
+            <p className="datetime"> {dateStr}</p>
         </div>
+        <p>{props.content}</p>
     </li>
+}
+
+type GqlCommentData = {
+    content: string,
+    datePosted: string,
+    author: {
+        name : string
+    },
+
+}
+
+/**
+ * Handles retrieving artwork comments from the server.
+ */
+function useComments() : {picture: string, comments: CommentProps[]} {
+    const {id} = useParams<{id:string}>()
+    const {loading, data, error} = useQuery(GET_DISCUSSION, {variables: {id}});
+    const hasNoData = loading || error
+    const comments: CommentProps[] = hasNoData ? [] : data.artwork.edges?.[0]
+    .node.comments.edges.map(({node}:{node : GqlCommentData}) => 
+    ({content: node.content, author: node.author.name, datePosted: new Date(node.datePosted)}))
+    console.log({data, error})
+    const picture = hasNoData ? "" : data.artwork.edges?.[0].node.pictures[0]
+    return {comments, picture}
 }
