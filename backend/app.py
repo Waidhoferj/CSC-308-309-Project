2be04d51bca3d0d96bc3a7b0f9d9c2ccf6e84759
Braden Michelson <63371610+Braden50@users.noreply.github.com>
2021-04-29 21:07:47 -0700

    testing a fix for deployed database connection

--- backend/app.py
+++ backend/app.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
     except:
         raise Exception("Missing one or more environmental variables")
     database_uri = "mongomock://localhost" if use_local_dev else f"mongodb+srv://{DB_USERNAME}:{DB_PASSWORD}@geoart.0gfam.mongodb.net/{DB_TESTING_NAME}?retryWrites=true&w=majority"  # secrets.DB_TESTING_URI
-    connect(host=database_uri)
+    connect(DB_TESTING_NAME, alias="default", host=database_uri)
     if use_local_dev:
         if execute_tests:
             testing_boot_up() # must use both local and testing tag to get here
