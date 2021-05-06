7c1f9a23e04a059af567bf8c05abc475a0eb002d
Braden Michelson <63371610+Braden50@users.noreply.github.com>
2021-05-06 15:27:40 -0700

    0.0.0/0 -> localhost in graphql-config.js

--- frontend/src/graphql-config.js
+++ frontend/src/graphql-config.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const uri =
   process.env.NODE_ENV === "production"
     ? "https://csc-309-geoart.herokuapp.com/graphql?"
-    : "http://0.0.0.0:8080/graphql?";
+    : "http://localhost:8080/graphql?";
 export const client = new ApolloClient({
   uri, // We will change this for production
   cache: new InMemoryCache(),
