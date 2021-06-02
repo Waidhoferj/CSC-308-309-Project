2cc96eee8613a33c5e81385cd9c6d7542bf9382b
Kyle <kshare@calpoly.edu>
2021-06-02 11:09:55 -0700

    Create scrollable art gallery

--- frontend/src/pages/ArtGallery/ArtGallery.tsx
+++ frontend/src/pages/ArtGallery/ArtGallery.tsx
@@ -1,60 +1,39 @@
+import "./ArtGallery.scss";
+
 import { GET_ARTWORK, ArtworkQueryData } from "../Artwork/gql";
 
-import { gql, useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import { ArrowLeft } from "react-feather";
-import { useParams, useHistory, Route, Switch } from "react-router-dom";
-import AwesomeSlider from 'react-awesome-slider';
-import 'react-awesome-slider/dist/styles.css';
-import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
-
+import { useParams, useHistory } from "react-router-dom";
+import ImageGallery from 'react-image-gallery';
+import "react-image-gallery/styles/scss/image-gallery.scss";
 
 export default function ArtGallery() {
     const history = useHistory();
     const { id } = useParams<{ id: string }>();
 
-
-    const { loading, error, data } = useQuery<ArtworkQueryData>(GET_ARTWORK, {
+    const { data } = useQuery<ArtworkQueryData>(GET_ARTWORK, {
       variables: { id },
     });
 
-    console.log("data is ");
-    console.log(data);
-
-    //const pictures = data?.artwork.edges[0].node.pictures;
-    // const pictures = [...data?.artwork.edges[0].node.pictures];
-
     const pictures = data?.artwork.edges[0].node.pictures;
 
-    //Why is the len of this always 1? 
-    console.log("pics len is" + pictures?.length);
-    console.log ("from server, pics len is" + data?.artwork.edges[0].node.pictures.length);
-
-
-    const picturelist = pictures?.map((picture, index) => {
-      return <div data-src={picture} key={`slider-${index}`}/>
+    const pictureObjs = pictures?.map((picture) => {
+      return {original: picture, thumbnail: picture}
     })
+
 
     return (
         <section className="ArtGallery">
           <nav>
-            <button className="wrapper" onClick={history.goBack}>
+            <button className="wrapper" onClick={history.goBack} style={{color:"black"}}>
               <ArrowLeft />
             </button>
           </nav>
 
-          {/*  
-          <AwesomeSlider animation="cubeAnimation">
-            {picturelist}
-          </AwesomeSlider>
-          */}
-
-          <AwesomeSlider animation="cubeAnimation">
-            {pictures?.map((picture) => 
-              <div data-src={picture} /> 
-            )}
-          </AwesomeSlider> 
-
-      
+          {pictureObjs instanceof Array &&
+          <ImageGallery items={pictureObjs} showThumbnails={true}/>
+          }
 
         </section>
     );
